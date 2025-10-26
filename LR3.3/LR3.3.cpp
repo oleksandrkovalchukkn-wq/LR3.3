321e4dbcc797b21c8d6cdbdcdae4eec2759427a2
Oleksandr Kovalchuk <oleksandr.kovalchuk.kn@dspu.edu.ua>
2025-10-26 15:43:54 +0200

    final

--- LR3.3/LR3.3.cpp
+++ LR3.3/LR3.3.cpp
@@ -1,39 +1,46 @@
-// Lab_03_3.cpp
-// < Ковальчук Олександр >
-// Лабораторна робота № 3.3
-// Розгалуження, задане графіком функції.
 // Варіант 18
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 int main()
 {
-	double x; // вхідний аргумент
 	double R1; // вхідний параметр
 	double R2; // вхідний параметр
 	double y; // результат обчислення виразу
+	double xp; // start
+	double xk; // end
+	double dx;
+
 	cout << "R1 = "; cin >> R1;
 	cout << "R2 = "; cin >> R2;
-	cout << "x = "; cin >> x;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
 
-	// розгалуження в повній формі
-	if (x <= -2 * R1)
-		y = -R1 * (x + 2 * R1);
-	else
-		if (-2 * R1 < x && x <= 0)
-			y = sqrt(pow(R1, 2) - pow((x - R1), 2));
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(10) << setprecision(3) << "y" << setw(10) << setprecision(3) << "|" << endl;
+	cout << "---------------------------" << endl;
+
+	while (xp <= xk)
+	{
+		if (xp <= -2 * R1)
+			y = -R1 * (xp + 2 * R1);
 		else
-			if (0 < x && x <= 2 * R2)
-				y = -sqrt(pow(R2, 2) - pow((x - R2), 2));
+			if (-2 * R1 < xp && xp <= 0)
+				y = sqrt(pow(R1, 2) - pow((xp - R1), 2));
 			else
-				if (2 * R2 < x && x <= 6)
-					y = -1 / (6 - 2 * R2) * (x - 2 * R2);
+				if (0 < xp && xp <= 2 * R2)
+					y = -sqrt(pow(R2, 2) - pow((xp - R2), 2));
 				else
-					y = -1;
+					if (2 * R2 < xp && xp <= 6)
+						y = -1 / (6 - 2 * R2) * (xp - 2 * R2);
+					else
+						y = -1;
+		cout << "|" << setw(10) << setprecision(3) << y << setw(10) << setprecision(3) << "|" << endl;
 
-	cout << endl;
-	cout << "y = " << y << endl;
-
-	cin.get();
+		xp += dx;
+	}
+	cout << "---------------------------" << endl;
 	return 0;
 }
